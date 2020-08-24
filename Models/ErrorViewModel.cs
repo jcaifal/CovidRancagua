c65c71678991694c41d95b1139b4e8696f31f520
jcaifal <juan.caifal@gmail.com>
2020-08-24 17:45:00 -0400

    Creación del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CovidRancagua.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
